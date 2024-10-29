cdcc2aef44d2551e7e6917f792e4df56c337dada
MohammadAllameh <rw1387rw@gmail.com>
2024-10-29 23:24:02 +0330

    Tutorial Project 0.02

---
+++ src/products/products.controller.ts
@@ -0,0 +1,52 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Put } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) { }
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    console.log(createProductDto)
+    if (createProductDto.price >= 1000) {
+      throw new Error('price must be less than 1000');
+    }
+    // return this.productsService.create(createProductDto);
+    return { message: "ok", data: createProductDto }
+  }
+
+  @Get()
+  //findAll(@Body createProductDto: CreateProductDto) {
+  //findAll(@Query createProductDto: CreateProductDto) {
+  //findAll(@Param('id') id:string) {
+
+  findAll() {
+    // return this.productsService.findAll();
+    return []
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    // return this.productsService.findOne(+id);
+    return { id }
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    // return this.productsService.update(+id, updateProductDto);
+    return { id }
+  }
+  @Put(':id')
+  updateAllitem(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    // return this.productsService.update(+id, updateProductDto);
+    return { id }
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(+id);
+    // return { id }
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    findAll() {
+        return [
+            { name: 'ali', username: 'ali123' },
+        ]
+    }
+    findUserById(id: number) {
+        return {
+            id,
+            name: 'ali',
+            username: 'ali123',
+        };
+    }
+    findUserByUserName(username: number) {
+        return {
+            id: 1,
+            name: 'ali',
+            username: 'ali123',
+        };
+    }
+}
